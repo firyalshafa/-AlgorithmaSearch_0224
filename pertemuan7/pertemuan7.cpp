a2ff9f8623eb15e0fc1c53370b9153931e7eedfd
firyalshafa <shafafiryal782@gmail.com>
2024-04-04 16:41:21 +0800

    membuat input

--- pertemuan7/pertemuan7.cpp
+++ pertemuan7/pertemuan7.cpp
@@ -25,6 +25,10 @@ void input()
     cout << "enter array elements\n";
     cout << "----------------------\n";
     for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
 
 
