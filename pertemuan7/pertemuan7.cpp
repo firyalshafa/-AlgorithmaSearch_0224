a70b975d5f936d0ecd3962a16e4023fdd351bd65
firyalshafa <shafafiryal782@gmail.com>
2024-04-04 17:04:21 +0800

    membuat step 4

--- pertemuan7/pertemuan7.cpp
+++ pertemuan7/pertemuan7.cpp
@@ -51,7 +51,9 @@ void linearsearch()
             if (arr[i] == item)
             {
                 cout << "\n" << item << " found at position " << (i + 1) << endl; // step 6
+                break;
             }
+            i++; // step 4 
         }
 
 
