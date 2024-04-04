b6266e56a8d856216c74e7535fe1da482c7d7465
firyalshafa <shafafiryal782@gmail.com>
2024-04-04 16:35:39 +0800

    membuat perulangan while

--- pertemuan7/pertemuan7.cpp
+++ pertemuan7/pertemuan7.cpp
@@ -8,6 +8,21 @@ int arr[20];
 int n;
 int i;
 
+void input()
+{
+    while (true)
+    {
+        cout << "enter the number of elements in the aaray:";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\narray should have minimum 1 and maximum 20 elements. \n\n";
+
+    }
+
+}
+
 
 int main()
 {
